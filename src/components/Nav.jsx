cfd78c151a8a09949112e33bd8a75091a27c53bf
Christa Favano <crstav@gmail.com>
2024-07-30 16:31:27 -0400

    Fixed class to className

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/logo.png'
 
 function Nav() {
     return (
-        <div className="row nav">
+        <div classNamegit a="row nav">
         <div className=" container">
             <img src={Logo} alt='AirBNB logo' />     
         </div>
