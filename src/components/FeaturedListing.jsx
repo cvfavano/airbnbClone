8fa5934b94a2dc781e6e6e43572ac37ebc4b9d40
Christa Favano <crstav@gmail.com>
2024-07-30 14:52:16 -0400

    add basic styling and logo file

---
+++ src/components/FeaturedListing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FeaturedListing() {
+
+    return (
+        <div>
+            <h1>Listing</h1>        
+        </div>
+    )
+}
+
+export default FeaturedListing
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../assets/logo.jpg'
+function Nav() {
+
+    return (
+        <div className="container">
+            <img src={Logo} alt='AirBNB logo'/>
+            <h1>nav</h1>        
+        </div>
+    )
+}
+
+export default Nav
